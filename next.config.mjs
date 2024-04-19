103c3c384a08ad912fc8a80c281bafd530cede33
Joun <urbanscratcher@gmail.com>
2024-04-19 19:50:29 +0900

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
-/** @type {import('next').NextConfig} */
-
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"],
     formats: ["image/webp"],
     minimumCacheTTL: 60,
     remotePatterns: [{ hostname: "*" }],
-  },
-  experimental: {
-    serverActions: true,
   },
 };
 
