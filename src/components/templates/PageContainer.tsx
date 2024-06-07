c48cda65d9fddb5ce463d5ee7ac8382806bd4190
Joun <urbanscratcher@gmail.com>
2024-06-07 18:08:08 +0900

    fixed whitespace of main page

--- src/components/templates/PageContainer.tsx
+++ src/components/templates/PageContainer.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 function PageContainer({ children }: PropsWithChildren) {
   return (
-    <main className="mx-auto my-0 flex max-w-[1440px] flex-col py-[24px] sm:py-[24px] lg:flex-row">
+    <main className="mx-auto flex max-w-[1440px] flex-col py-[24px] sm:py-[32px] lg:flex-row">
       {children}
     </main>
   );
