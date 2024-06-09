b71a63564bca89e5157413063c7b4c9720aa2127
Joun <urbanscratcher@gmail.com>
2024-06-10 03:11:39 +0900

    wrapped suspense

--- src/app/insights/page.tsx
+++ src/app/insights/page.tsx
@@ -5,12 +5,15 @@ import RightSideContainer from "@/components/atoms/RightSideContainer";
 import InspirationAside from "@/components/organisms/InspirationAside";
 import PageDivider from "@/components/organisms/PageDivider";
 import WhatsTrendingAside from "@/components/organisms/WhatsTrendingAside";
+import { Suspense } from "react";
 
 function InsightsPage() {
   return (
     <PageContainer>
       <MainSideContainer>
-        <InsightsByTopicSection />
+        <Suspense>
+          <InsightsByTopicSection />
+        </Suspense>
       </MainSideContainer>
       <PageDivider />
       <RightSideContainer>
