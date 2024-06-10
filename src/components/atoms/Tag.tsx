cbb0af7e2568d756ff66dba5ed96241e498678d3
Joun <urbanscratcher@gmail.com>
2024-06-11 02:12:47 +0900

    tag border 1.5px

--- src/components/atoms/Tag.tsx
+++ src/components/atoms/Tag.tsx
@@ -15,7 +15,7 @@ function Tag({ tagName, white, className }: TagProps) {
       }}
       className={`text-button h-fit w-fit rounded-full bg-transparent px-4 py-1 uppercase
       hover:border-sky-700 hover:bg-sky-700 hover:text-white
-      ${white ? "border-2 border-white font-medium text-white" : "border-2 border-sky-700 font-bold text-sky-700"}
+      ${white ? "border-[1.5px] border-white font-medium text-white" : "border-[1.5px] border-sky-700 font-bold text-sky-700"}
       ${className || ""}
       `}
     >
